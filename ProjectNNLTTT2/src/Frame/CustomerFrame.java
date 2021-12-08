3a637f3f8cd9376d2bda70fc8c8bf4598dd2c1e7
cotrang20012 <54300415+cotrang20012@users.noreply.github.com>
2021-12-08 22:14:26 +0700

    Final

--- ProjectNNLTTT2/src/Frame/CustomerFrame.java
+++ ProjectNNLTTT2/src/Frame/CustomerFrame.java
@@ -232,8 +232,10 @@ public class CustomerFrame extends JFrame {
 					int delConfirmed = JOptionPane.showConfirmDialog(null, "Bạn có chắc chắn muốn xóa không?");
 					if (delConfirmed == 0) {
 						CustomerModel cust = getCust();
-						CustomerModel.deleteCust(cust);
-						JOptionPane.showMessageDialog(contentPane, "Xóa thành công!");
+						if(CustomerModel.deleteCust(cust)) {
+							JOptionPane.showMessageDialog(contentPane, "Xóa thành công!");
+						}
+						JOptionPane.showMessageDialog(contentPane, "Xóa không thành công!");
 						loadData();
 					}
 				}
@@ -256,11 +258,13 @@ public class CustomerFrame extends JFrame {
 			       int selectedRowIndex = tableCustomer.getSelectedRow();
 			       
 			        // set the selected row data into jtextfields
-			       txt_id.setText(model.getValueAt(selectedRowIndex, 0).toString());
-			       txt_name.setText(model.getValueAt(selectedRowIndex, 1).toString());
-			       txt_cmnd.setText(model.getValueAt(selectedRowIndex, 2).toString());
-			       txt_addr.setText(model.getValueAt(selectedRowIndex, 3).toString());
-			       txt_phone.setText(model.getValueAt(selectedRowIndex, 4).toString());			       
+			       if(selectedRowIndex != -1) {
+			    	   txt_id.setText(model.getValueAt(selectedRowIndex, 0).toString());
+				       txt_name.setText(model.getValueAt(selectedRowIndex, 1).toString());
+				       txt_cmnd.setText(model.getValueAt(selectedRowIndex, 2).toString());
+				       txt_addr.setText(model.getValueAt(selectedRowIndex, 3).toString());
+				       txt_phone.setText(model.getValueAt(selectedRowIndex, 4).toString());		
+			       }	       
 			}
 		});
 		if(Global.acc.getUsertype().equals("SALES")) {
---
+++ README.txt
@@ -0,0 +1,4 @@
+- Các ứng dụng cần để chạy project: 
++ jdatepicker-1.3.4
++ mysql-connector-java-8.0.27
++ Cơ sở dữ liệu MySQL.
